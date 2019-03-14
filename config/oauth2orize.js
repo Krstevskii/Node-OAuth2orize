0cd21e5d4811ea1554b2ed0c833864766649d78e
Krstevskii <mr.krstevski@live.com>
2019-03-14 16:53:43 +0100

    Exchange grant for token service add

--- config/oauth2orize.js
+++ config/oauth2orize.js
@@ -9,6 +9,7 @@ const server = oauth2orize.createServer();
 server.grant(oauth2orize.grant.code({
     scopeSeparator: [' ', ',']
 }, (application, redirectURI, user, ares, done) => {
+
     const grant = new GrantCode({
         application: application,
         user: user,
@@ -18,4 +19,27 @@ server.grant(oauth2orize.grant.code({
     grant.save()
         .then(grnt => done(null, grnt.code), err => done(err))
         .catch(err => done(err));
+
+}));
+
+server.exchange(oauth2orize.exchange.code({
+    userProperty: 'app'
+}, (application, code, redirectURI, done) => {
+
+    GrantCode.findOne({code: code})
+        .then(grant => {
+            if (grant && grant.active && grant.application == application.id) {
+                const token = new AccessToken({
+                    application: grant.application,
+                    user: grant.user,
+                    grant: grant,
+                    scope: grant.scope
+                });
+
+                token.save()
+                    .then(tkn => done(null, tkn.token), err => done(err))
+                    .catch(err => done(err));
+            }
+        })
+
 }));
