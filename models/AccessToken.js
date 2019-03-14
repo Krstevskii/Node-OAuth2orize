aa801e3aca29929d0699b59ac6b3bbc7da2cb531
Krstevskii <mr.krstevski@live.com>
2019-03-14 16:41:31 +0100

    AccessTokenSchema added

---
+++ models/AccessToken.js
@@ -0,0 +1,49 @@
+const mongoose = require('mongoose');
+const uid = require('uid-safe');
+
+const Schema = mongoose.Schema;
+
+const AccessTokenSchema = new Schema({
+
+    token: {
+        type: String, unique: true, default: async function () {
+            return await uid(256);
+        }
+    },
+
+    user: {
+        type: Schema.Types.ObjectId, ref: 'User'
+    },
+
+    application: {
+        type: Schema.Types.ObjectId, ref: 'Application'
+    },
+
+    grant: {
+        type: Schema.Types.ObjectId, ref: 'GrantCode'
+    },
+
+    scope: [{type: String}],
+
+    expires: {
+        type: Date, default: function () {
+            const today = new Date();
+            const length = 60;
+
+            return new Date(today.getTime() + length * 60000);
+        }
+    },
+
+    active: {
+        type: Boolean, get: function (value) {
+            if (expires < new Date() || !value) {
+                return false;
+            } else {
+                return value;
+            }
+        }, default: true
+    }
+
+});
+
+module.exports = AccessToken = mongoose.model('AccessToken', AccessTokenSchema);
